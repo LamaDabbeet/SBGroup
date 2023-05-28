1ab0c73dd89760d7f15b818b5313cae774396f38
LamaDabbeet <lamadabbeet@gmail.com>
2023-05-28 22:01:31 +0400

    enhancments

--- pages/index.tsx
+++ pages/index.tsx
@@ -59,7 +59,7 @@ export default function Home() {
           modules={[Navigation]}
           className="hover:border-b-4 hover:border-b-secondary border-b-white border-b-4 transition duration-500"
           autoplay={{
-            delay: 2500,
+            delay: 5000,
             pauseOnMouseEnter: true,
           }}
         >
